e4952a160603ae488d8aad7eab5e554cac437e01
Tara Trody <shantara.trody@gmail.com>
2021-02-12 00:01:34 -0500

    add form event listener

--- forms-layout/forms-grid/form-grid.js
+++ forms-layout/forms-grid/form-grid.js
@@ -1,47 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Document</title>
-</head>
-<body>
-<form id="myform" action="get">
-  <fieldset>
-    <legend>Your web development skillset</legend>
-    
-    <div class="formgrid">
-      
-      <input id="name" name="name" type="text" />
-      <label for="name">name</label>
-    
-      <select id="experience" name="experience">
-        <option value="1">1 year or less</option>
-        <option value="2">2 years</option>
-        <option value="3">3 - 4 years</option>
-        <option value="5">5 years or more</option>
-      </select>
-      <label for="experience">experience</label>
-    
-      <input id="html" name="html" type="checkbox" />
-      <label for="html">HTML</label>
-    
-      <input id="css" name="css" type="checkbox" />
-      <label for="css">CSS</label>
-    
-      <input id="javascript" name="javascript" type="checkbox" />
-      <label for="javascript">JavaScript</label>
-      
-      <textarea id="skills" name="skills" rows="5" cols="20"></textarea>
-      <label for="skills">other skills</label>
-      
-      <button type="submit">SUBMIT</button>
-      
-    </div>
-    
-  </fieldset>
-</form>
-
-<p>This layout applies CSS Grid in all browsers which support it. It adapts well to any font or screen sizing.</p>
-</body>
-</html>
+document.getElementById('myform').addEventListener(
+  'submit',
+  e => {
+    e.preventDefault();
+    alert('Nothing to see here.\nPlease move along.');
+  }
+);
